f427e4123ac51bcf17bcb901146411a229949ba2
reda97dz <rbaka79@gmail.com>
2021-08-22 18:55:18 +0100

    Update games.js

--- src/Services/games.js
+++ src/Services/games.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 const baseUrl = 'https://api.football-data.org/v2/teams/86/matches'
 
 const upcomingGames = () => {
-    const request = axios.get(baseUrl+'?status=SCHEDULED', {headers : {'X-Auth-Token' : '3231994280e54acbb604868cca7988a1'}})
+    const request = axios.get(baseUrl+'?status=SCHEDULED', {headers : {'X-Auth-Token' : '/'}})
     return request.then(response => response.data.matches)
 }
 
 const finishedGames = () => {
-    const request = axios.get(baseUrl+'?status=FINISHED', {headers : {'X-Auth-Token' : '3231994280e54acbb604868cca7988a1'}})
+    const request = axios.get(baseUrl+'?status=FINISHED', {headers : {'X-Auth-Token' : '/'}})
     return request.then(response => response.data.matches)
 }
 
